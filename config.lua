f2ca44db8097f32be0bfdf23629bdb32a12d02bf
ManWithA1000Names <loena.phivos2010@gmail.com>
2022-09-08 13:37:25 +0300

    fix(lsp): extend skipped_server list instead of overwriting it

--- config.lua
+++ config.lua
@@ -20,19 +20,14 @@ vim.g.tokyonight_style = "night"
 vim.cmd("autocmd VimLeave,VimSuspend * set guicursor=a:ver90")
 
 -- config vim-go
-vim.cmd([[
-  let g:go_code_completion_enabled = 0
-  let g:go_play_browser_command = 'xdg-open %URL% &'
-  let g:go_fmt_autosave = 0
-  let g:go_echo_go_info = 0
-  let g:go_addtags_transform = 'camelcase'
-]])
+vim.g.go_code_completion_enabled = 0
+vim.g.go_fmt_autosave = 0
+vim.g.go_echo_go_info = 0
+vim.g.go_addtags_transform = "camelcase"
 
 -- gui stuff
-vim.cmd([[
-  let g:neovide_refresh_rate=120
-  set guifont=FiraCode\ Nerd\ Font:h10
-]])
+vim.g.neovide_refresh_rate = 120
+vim.opt["guifont"] = "FiraCode Nerd Font:h10"
 
 --[[
  ██ ▄█▀▓█████▓██   ██▓ ▄▄▄▄    ██▓ ███▄    █ ▓█████▄  ██▓ ███▄    █   ▄████   ██████ 
@@ -75,6 +70,7 @@ lvim.keys.normal_mode["<Return>"] = "@a"
 lvim.keys.normal_mode["<Return>"] = "@w"
 -- Completing VIM
 lvim.keys.normal_mode["Y"] = "y$"
+lvim.keys.normal_mode['"'] = ":nohl<CR>"
 -- Navigation
 lvim.keys.normal_mode[","] = ":HopWord<CR>"
 -- Terminal
@@ -207,8 +203,7 @@ linters.setup({
 
 -- config extensions
 lvim.lsp.diagnostics.virtual_text = false
-
-lvim.lsp.automatic_configuration.skipped_servers = { "rust" }
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "rust" })
 
 --[[
  ██▓███   ██▓     █    ██   ▄████  ██▓ ███▄    █   ██████ 
